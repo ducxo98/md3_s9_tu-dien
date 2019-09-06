ce60534240191d2e47421534441838e441275196
duc <ducxo98@gmail.com>
2019-09-06 11:08:03 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'BT-app-tra-cuu-tu-dien';
+}
